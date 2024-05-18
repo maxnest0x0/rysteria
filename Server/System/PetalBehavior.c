f8a62c55b3cd24acfb2a0c32487dd6cc8ed5e2a1
cat <152820768+kit2d2alt@users.noreply.github.com>
2024-05-18 12:50:58 -0700

    Update PetalBehavior.c

--- Server/System/PetalBehavior.c
+++ Server/System/PetalBehavior.c
@@ -244,7 +244,7 @@ static void system_flower_petal_movement_logic(
         {
             struct rr_component_health *flower_health =
                 rr_simulation_get_health(simulation, player_info->flower_id);
-            float heal = 6 * RR_PETAL_RARITY_SCALE[petal->rarity].heal;
+            float heal = 6.5 * RR_PETAL_RARITY_SCALE[petal->rarity].heal;
             if (flower_health->health < flower_health->max_health)
             {
                 struct rr_vector delta = {
