f0a289cb593b918378258c2497f2b8a71d8e8fa8
cat <152820768+kit2d2alt@users.noreply.github.com>
2024-05-22 16:07:49 -0700

    Pectinodon Spritesheet Rendering
    btw with garden mobs just update `&mob_sprites[x]` values to one more than they currently are

--- Client/Assets/RenderMobs.c
+++ Client/Assets/RenderMobs.c
@@ -196,6 +196,7 @@ void rr_renderer_draw_mob(struct rr_renderer *renderer, uint8_t id,
         render_sprite(renderer, id, 0, flags);
         break;
     case rr_mob_id_hornet:
+    case rr_mob_id_pectinodon:
     case rr_mob_id_honeybee:
         rr_renderer_scale(renderer, 0.2f);
         /*
@@ -422,6 +423,11 @@ void rr_renderer_mob_cache_init()
         &mob_sprites[11], NULL, 240, 144, rr_edmontosaurus_head_draw, 339, 192,
         rr_edmontosaurus_body_draw, 240, 240, rr_edmontosaurus_leg1_draw, 240,
         240, rr_edmontosaurus_leg2_draw, 336, 192, rr_edmontosaurus_tail_draw,
+        0);
+    rr_renderer_spritesheet_init(
+        &mob_sprites[12], NULL, 240, 144, rr_pectinodon_head_draw, 336, 192,
+        rr_pectinodon_body_draw, 240, 144, rr_pectinodon_wing1_draw, 240,
+        144, rr_pectinodon_wing2_draw, 336, 192, rr_pectinodon_tail_draw,
         0);
     // garden
     rr_renderer_spritesheet_init(&mob_sprites[12], NULL, 192, 192,
