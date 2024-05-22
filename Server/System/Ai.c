7add22c2d639483953852a03b69360e0c5cbba57
cat <152820768+kit2d2alt@users.noreply.github.com>
2024-05-22 15:06:33 -0700

    Update Ai.c

--- Server/System/Ai.c
+++ Server/System/Ai.c
@@ -109,6 +109,14 @@ static void system_for_each(EntityIdx entity, void *simulation)
             tick_ai_neutral_default(entity, this, RR_PLAYER_SPEED *
                                     (1.5 - mob->rarity * 0.05));
         break;
+    case rr_mob_id_pectinodon:
+        if (mob->rarity >= rr_rarity_id_epic) // Change to rare, set as epic because due to high body damage may be a pain at lower rarities before speed progressively diminishes.
+            tick_ai_aggro_default(entity, this, RR_PLAYER_SPEED *
+                                  (1.4 - mob->rarity * 0.09));
+        else
+            tick_ai_neutral_default(entity, this, RR_PLAYER_SPEED *
+                                    (1.4 - mob->rarity * 0.09));
+        break;
     case rr_mob_id_pachycephalosaurus:
         tick_ai_aggro_pachycephalosaurus(entity, this);
         break;
